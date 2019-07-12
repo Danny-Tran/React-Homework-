1d7bf0ffdd0fea182750e2f0b6f7b2e4bb00ab4b
Danny-Tran <truong_tran@live.com>
2019-07-12 20:39:59 +0000

    got externally load from Unsplash

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,23 +1,94 @@
-import React from "react";
-import PropTypes from "prop-types";
+// import React from "react";
+// import axios from "axios";
+// // import PropTypes from "prop-types";
 
-// GALLERY COMPONENT
-class Gallery extends React.Component {
-  renderImage(imageUrl) {
-    return (
-      <span>
-        <img src={imageUrl} />
-      </span>
-    );
-  }
+// // // GALLERY COMPONENT
+// class Gallery extends React.Component {
+//   renderImage(imageUrl) {
+//     return (
+//       <span>
+//         <img src={imageUrl} />
+//       </span>
+//     );
+//   }
 
-  render() {
-    return (
-      <div className="images" >
-        {this.props.imageUrls.map(imageUrl => this.renderImage(imageUrl))}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="images" >
+//         {this.props.imageUrls.map(imageUrl => this.renderImage(imageUrl))}
+//       </div>
+//     );
+//   }
+// componentDidMount(){
 
-export default Gallery;
+//   UnsplashImage = ({ url, key }) => (
+//     <div className="image-item" key={key} >
+//     <img src={url} />
+//   </div>
+// );
+
+//  Collage = () => {
+//    const [images, setImages] = React.useState([]);
+//   const [loaded, setIsLoaded] = React.useState(false);
+  
+//   React.useEffect(() => {
+//     fetchImages();
+//   }, []);
+
+//   const fetchImages = (count = 10) => {
+//     const apiRoot = "https://api.unsplash.com";
+//     const accessKey =
+//     "a22f61e98da4efa25d8860e77a91a596867dd335ecdf7feb12e086943db9565a";
+    
+//     axios
+//     .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+//     .then(res => {
+//       setImages([...images, ...res.data]);
+//       setIsLoaded(true);
+      
+//       console.log(images);
+//       });
+//     };
+//   }
+  
+//   return (
+//     <div className="hero is-fullheight is-bold is-info">
+//       <div className="hero-body">
+//         <div className="container">
+//           <div className="header content">
+//             <h2 className="subtitle is-6">Code Challenge #16</h2>
+//             <h1 className="title is-1">
+//               Infinite Scroll Unsplash Code Challenge
+//             </h1>
+//           </div>
+
+//           <InfiniteScroll
+//             dataLength={images}
+//             next={() => fetchImages(5)}
+//             hasMore={true}
+//             loader={
+//               <img
+//               src="https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif"
+//               alt="loading"
+//               />
+//             }
+//             >
+//             <div className="image-grid" style={{ marginTop: "30px" }}>
+//               {loaded
+//                 ? images.map((image, index) => (
+//                   <UnsplashImage
+//                   url={image.urls.regular}
+//                   key={index}
+//                   />
+//                   ))
+//                   : ""}
+//             </div>
+//           </InfiniteScroll>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// }
+
+// export default Gallery;
